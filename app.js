94fb2e4e1a22e4281900ba834e62eb5580e4d052
Rafael Pawlos <rafael.pawlos@gmail.com>
2016-11-14 04:13:39 +0100

    First attempt to synchronize charts pointer

--- app.js
+++ app.js
@@ -169,7 +169,10 @@
             title: {
               text: 'Time [ms]'
             },
-            crosshair: true
+            crosshair: true,
+            events: {
+              setExtremes: syncExtremes
+            }
           },
           yAxis: {
             title: {
@@ -240,7 +243,10 @@
             title: {
               text: 'Time [ms]'
             },
-            crosshair: true
+            crosshair: true,
+            events: {
+              setExtremes: syncExtremes
+            }
           },
           yAxis: {
             title: {
@@ -338,3 +344,56 @@
   var chartsContainer = doc.getElementById('charts-container');
   Object.create(stickyMaker).init(chartsContainer);
 })(document);
+
+/**
+ * Synchronize charts pointer
+ */
+var chartsContainer = document.getElementById('charts-container');
+chartsContainer.addEventListener('mousemove', function (e) {
+  var chart,
+      point,
+      i,
+      event;
+
+  for (i = 0; i < Highcharts.charts.length; i++) {
+    chart = Highcharts.charts[i];
+    event = chart.pointer.normalize(e.originalEvent); // Find coordinates within the chart
+    point = chart.series[0].searchPoint(event, true); // Get the hovered point
+
+    if (point) {
+      point.highlight(e);
+    }
+  }
+});
+/**
+ * Override the reset function, we don't need to hide the tooltips and crosshairs.
+ */
+Highcharts.Pointer.prototype.reset = function () {
+  return undefined;
+};
+
+/**
+ * Highlight a point by showing tooltip, setting hover state and draw crosshair
+ */
+Highcharts.Point.prototype.highlight = function (event) {
+  this.onMouseOver(); // Show the hover marker
+  this.series.chart.tooltip.refresh(this); // Show the tooltip
+  this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+};
+
+/**
+ * Synchronize zooming through the setExtremes event handler.
+ */
+function syncExtremes(e) {
+  var thisChart = this.chart;
+
+  if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
+    Highcharts.each(Highcharts.charts, function (chart) {
+      if (chart !== thisChart) {
+        if (chart.xAxis[0].setExtremes) { // It is null while updating
+          chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {trigger: 'syncExtremes'});
+        }
+      }
+    });
+  }
+}
